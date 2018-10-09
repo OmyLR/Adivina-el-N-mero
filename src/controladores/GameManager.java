1809c94d1ba5ab57cdf10ace5a2868a1e7da8233
OmyLR <omysam@gmail.com>
2018-10-09 13:04:06 +0100

    C1 09/10/2018
    Realizo pequeñas modificaciones al index y comienzo a realizar el servlet

--- src/controladores/GameManager.java
+++ src/controladores/GameManager.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import utiles.Utiles;
+
 /**
  * Servlet implementation class Jugar
  */
@@ -29,7 +31,16 @@ public class GameManager extends HttpServlet {
 		// TODO Auto-generated method stub
 		Juego juego = (Juego)request.getSession().getAttribute("Partida");
 		if(juego == null) {
-			
+			String min =  request.getParameter("min");
+			String max = request.getParameter("max");
+			try {
+				if(Utiles.controlarNumeros(min, max)) {
+					
+				}
+			} catch(Error e) {
+				String[] err = e.getMessage().split("$");
+				mostrarError(response, err[0], err[1]);
+			}
 		} else {
 			
 		}
@@ -41,6 +52,10 @@ public class GameManager extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+	}
+	
+	private void mostrarError(HttpServletResponse response, String err, String ruta) throws IOException{
+		response.sendRedirect(ruta);
 	}
 
 }
--- src/utiles/Utiles.java
+++ src/utiles/Utiles.java
@@ -12,13 +12,13 @@ public class Utiles {
 	public static boolean controlarNumeros(String numA, String numB) {
 		
 		if(numA.trim() == null || numB.trim() == null) {
-			throw new Error("No has insertado un número");
+			throw new Error("No has insertado un número$/");
 		}
 		try {
 			int a = Integer.parseInt(numA);
 			int b = Integer.parseInt(numB);
 		}catch(NumberFormatException e) {
-			throw new Error("Has insertado algo que no es un número");
+			throw new Error("Has insertado algo que no es un número$/");
 		}
 		return true;
 	}
